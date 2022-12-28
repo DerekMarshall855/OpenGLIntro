9958967e6668dd67e22b72918c8b62851015c486
DerekMarshall855 <derekmarshall855@gmail.com>
2022-12-28 15:20:19 -0500

    Swap to core profile for opengl, create and use vao for our geometry

--- src/Application.cpp
+++ src/Application.cpp
@@ -116,6 +116,11 @@ int main(void)
     if (!glfwInit())
         return -1;
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    // Vertex Array Object REQUIRED to be explicitly created on Core Profile
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     if (!window)
@@ -145,7 +150,17 @@ int main(void)
         0, 1, 2,
         2, 3, 0
     };
-
+    
+    /*
+     Two common methods for vao:
+        - Use global vao where you rebind buffers to it before draw
+        - Have one vao for each piece of geometry
+     The better one depends on situation (maybe, 2017 vid lol), but opengl recommends you use them
+     Best to run tests and see whether global vao or object specific vao is faster based on prod env
+    */
+    unsigned int vao;
+    GLCall(glGenVertexArrays(1, &vao));
+    GLCall(glBindVertexArray(vao));
     // Note: Use docs.gl to figure this stuff out, great docs
     unsigned int buffer;
     GLCall(glGenBuffers(1, &buffer));
@@ -153,6 +168,7 @@ int main(void)
     GLCall(glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW));
 
     GLCall(glEnableVertexAttribArray(0));
+    // This line links the vertex array and the buffer (index 0 [first param] of vao is bound to currently bound buffer)
     GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, (const void*)0));
     
     // Can use unsigned char to save memory, but limits to 0-255 indicies
@@ -170,6 +186,12 @@ int main(void)
     GLCall(int location = glGetUniformLocation(shader, "u_Color"));
     ASSERT(location != -1);  // If location -1 the program couldn't find the uniform (either doesnt exist or is unused)
     GLCall(glUniform4f(location, 0.8f, 0.3f, 0.8f, 1.0f));
+
+    GLCall(glBindVertexArray(0));
+    GLCall(glUseProgram(0));
+    GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
+    GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
+
     float r = 0.0f;
     float increment = 0.05f;
     /* Loop until the user closes the window */
@@ -178,16 +200,21 @@ int main(void)
         /* Render here */
         GLCall(glClear(GL_COLOR_BUFFER_BIT));
 
+		GLCall(glUseProgram(shader));
         // Uniforms are set PER DRAW
         GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
+
+        GLCall(glBindVertexArray(vao));
+		GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
         // Draws CURRENTLY BOUND buffer
         // MUST USE UNSIGNED INT IN A BUFFER
         GLCall(glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(unsigned int), GL_UNSIGNED_INT, nullptr));
 
+        // Can clamp here but won't since it's quick practice
         if (r > 1.0f)
-            increment = -0.05f;
+            increment = -0.01f;
         else if (r < 0.0f)
-            increment = 0.05f;
+            increment = 0.01f;
 
         r += increment;
 
